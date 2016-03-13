08d3b274d5213c100fe79ed9ea35cb743f619a84
richardgmartin <richardgeorgemartin@gmail.com>
2016-03-13 19:26:55 -0400

    completed Task2: added UIAlertController to build list

--- DreamListManager/ListViewController.m
+++ DreamListManager/ListViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+@property NSMutableArray *titlesArray;
 
 
 @end
@@ -20,10 +21,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.titlesArray = [[NSMutableArray alloc] init];
+
+}
+
+-(void)presentDreamEntry {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Enter New List Item" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Dream Title";
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        UITextField *textFieldOne = alertController.textFields[0];
+        [self.titlesArray addObject:textFieldOne.text];
+        [self.tableView reloadData];
+    }];
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:saveAction];
+    
+    [self presentViewController:alertController animated:true completion:nil];
+    
 }
 
 - (IBAction)onAddButtonTapped:(UIBarButtonItem *)sender {
+    
+    [self presentDreamEntry];
 }
 
 
@@ -35,11 +61,13 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     
+    cell.textLabel.text = [self.titlesArray objectAtIndex:indexPath.row];
+    
     return cell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return self.titlesArray.count;
     
 }
 
